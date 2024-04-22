7cf74f0a49f17e51bd5d47e2a0973e157eb61a10
Julio Cesar Goncalves Conceicao <julioconceicao.gtr@gmail.com>
2024-04-22 19:52:01 -0300

    add project

---
+++ README.md
@@ -0,0 +1,1 @@
+# simpligestor_projeto_eniac
---
+++ my-app/App.js
@@ -0,0 +1,17 @@
+import { View, Text, StyleSheet, Image } from 'react-native';
+import Icon from 'react-native-vector-icons/Entypo'
+import TelaInicial from './src/screens/TelaLogin/TelaLogin'
+import HomePage from './src/screens/Home/HomePage'
+
+export default function App() {
+  return (
+    <>
+      <TelaInicial />
+
+      {/* <HomePage /> */}
+
+
+    </>
+  );
+}
+

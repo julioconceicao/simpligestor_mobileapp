2356c72413c0171f097c22cdb23c15c8b93a37dd
Julio Cesar Goncalves Conceicao <julioconceicao.gtr@gmail.com>
2024-04-22 20:27:08 -0300

    delete: node files + chore: separar style de jsx

---
+++ my-app/src/screens/Home/HomePageStyle.js
@@ -0,0 +1,12 @@
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 20,
+      backgroundColor: 'whitesmoke',
+    },
+    logo: {
+      marginLeft: '11%',
+      width: 300,
+      height: 300,
+    }
+  });

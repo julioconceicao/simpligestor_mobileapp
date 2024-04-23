2be9f5bf39351ac742c2bdf6f48946b1f260858f
joaopedronqt <joao.pedro-00@outlook.com>
2024-04-23 16:37:08 -0300

    Tela de login finalizada e Splash screen pronta

--- my-app/src/screens/TelaLogin/TelaLogin.jsx
+++ my-app/src/screens/TelaLogin/TelaLogin.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react';
-import { View, Text, StyleSheet, Image, TextInput, Pressable  } from 'react-native';
-import  Icon  from 'react-native-vector-icons/Entypo'
+import { View, Text, StyleSheet, Image, TextInput, Pressable, TouchableOpacity   } from 'react-native';
+import { styles } from './TelaLoginStyle';
+
+import SwipeButton from '../../components/swipeButton/SwipeButton.jsx';
 
 export default function TelaLogin() {
-
-
   return (
     <View style={styles.container}>
       <Image
@@ -13,57 +13,16 @@ export default function TelaLogin() {
       />
       
       <TextInput style={styles.textInputEmail} placeholder='Email' />
-      <TextInput style={styles.textInputSenha} placeholder='Senha' />
+      <TextInput style={styles.textInputSenha} placeholder='Senha' secureTextEntry={true} />
 
-      <Pressable style={styles.botaoEntrar} >
-        <Text>Acessar</Text>
-      </Pressable>
-
-
+      <TouchableOpacity style={styles.botaoEntrar} >
+        <Text style={styles.text}>Acessar</Text>
+      </TouchableOpacity>
+      
+      <SwipeButton/>
+      
     </View>
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-    backgroundColor: 'whitesmoke',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: 'white',
-  },
-  logo: {
-    marginLeft: '11%',
-    width: 300,
-    height: 300,
-  },
-  textInputEmail: {
-    width: '100%',
-    height: 50,
-    backgroundColor: '#dcdcdc',
-    borderRadius: 30,
-    paddingLeft: 15
-  },
-  textInputSenha: {
-    width: '100%',
-    height: 50,
-    backgroundColor: '#dcdcdc',
-    borderRadius: 30,
-    paddingLeft: 15,
-    marginTop: 15
-  },
-  botaoEntrar: {
-    marginTop: 45,
-    height: 50,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 30,
-    backgroundColor: '#3E5DFF',
-    
-  }
-
-  
-});
---
+++ my-app/src/screens/TelaLogin/TelaLoginStyle.js
@@ -0,0 +1,50 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: 'whitesmoke',
+      },
+      title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'white',
+      },
+      logo: {
+        marginLeft: '11%',
+        width: 300,
+        height: 300,
+      },
+      textInputEmail: {
+        width: '100%',
+        height: 50,
+        backgroundColor: '#dcdcdc',
+        borderRadius: 30,
+        paddingLeft: 15
+      },
+      textInputSenha: {
+        width: '100%',
+        height: 50,
+        backgroundColor: '#dcdcdc',
+        borderRadius: 30,
+        paddingLeft: 15,
+        marginTop: 15
+      },
+      botaoEntrar: {
+        marginTop: 45,
+        height: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 30,
+        backgroundColor: '#3E5DFF',
+        
+      },
+      text: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: 'white',
+      },
+
+      
+});
